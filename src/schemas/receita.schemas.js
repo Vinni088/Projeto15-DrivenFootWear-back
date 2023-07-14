458458c57b38e8af5dc3fd9b98527f15b13cf237
beatrizcarolina <beatrizcarolinasr@gmail.com>
2023-07-14 00:03:44 -0300

    remove unused files

--- src/schemas/receita.schemas.js
+++
@@ -1,7 +0,0 @@
-import Joi from "joi"
-
-export const schemaReceita = Joi.object({
-	titulo: Joi.string().required(),
-	ingredientes: Joi.string().required(),
-	preparo: Joi.string().required().min(5).max(200)
-})
--- src/schemas/usuario.schemas.js
+++
@@ -1,7 +0,0 @@
-import Joi from "joi"
-
-export const schemaUsuario = Joi.object({
-    nome: Joi.string().required(),
-    email: Joi.string().email().required(),
-    senha: Joi.string().required().min(3)
-});
