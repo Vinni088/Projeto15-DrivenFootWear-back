3540850e10b6cc1274fca46edab24781a1def802
beatrizcarolina <beatrizcarolinasr@gmail.com>
2023-07-15 12:51:47 -0300

    configure cart routes

--- src/routes/buy.routes.js
+++ src/routes/buy.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express"
-import { getProducts, getProductsById } from "../controllers/products.controller.js"
+import { getProducts, getProductsById, getCart, addItemCart, removeItemCart } from "../controllers/products.controller.js"
 import { validateSchema } from "../middlewares/validateSchema.js"
 import { validateAuth } from "../middlewares/validateAuth.js"
 
@@ -8,11 +8,9 @@ const buyRouter = Router();
 buyRouter.use(validateAuth);
 
 buyRouter.get("/products", validateAuth, getProducts);
-buyRouter.get("/products/:id", validateAuth, getProductsById)
-//buyRouter.post("/receitas", validateSchema(schemaReceita), createRecipe);
-//buyRouter.delete("/receitas/:id", deleteRecipe);
-//buyRouter.delete("/receitas/muitas/:filtroIngredientes", deleteRecipesByIngredients);
-//buyRouter.put("/receitas/:id", validateSchema(schemaReceita), editRecipe);
-//buyRouter.put("/receitas/muitas/:filtroIngredientes", editRecipesByIngridients);
+buyRouter.get("/products/:id", validateAuth, getProductsById);
+buyRouter.get("/cart", validateAuth, getCart);
+buyRouter.post("/cart/:productId", validateAuth, addItemCart);
+buyRouter.delete("/cart/:productId", validateAuth, removeItemCart);
 
 export default buyRouter
